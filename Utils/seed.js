de07080dbd7cba1539b3321062ec7b21b4dcdd3e
BrielleBroadt <brielleb.broadt@gmail.com>
2023-11-08 11:49:19 -0500

    established friend relationship and saving user

--- Utils/seed.js
+++ Utils/seed.js
@@ -10,28 +10,47 @@ const seedData = async () => {
         if (existingUser.length === 0){
             const UserA = await User.create({
                 username: "UserA",
-                email: "userA@test.com",
+                email: "UserA@test.com",
                 thought: [],
                 friends: [],
             });
             const UserB = await User.create({
                 username: "userB",
-                email: "userB@test.com",
+                email: "UserB@test.com",
                 thought: [],
                 friends: [],
             });
             const UserC = await User.create({
-                username: "userC",
-                email: "userC@test.com",
+                username: "UserC",
+                email: "UserC@test.com",
                 thought: [],
                 friends: [],
             });
             const UserD = await User.create({
-                username: "userD",
-                email: "userD@test.com",
+                username: "UserD",
+                email: "UserD@test.com",
                 thought: [],
                 friends: [],
             });
+// establishing friends relationship 
+UserA.friends.push(UserB, UserC, UserD);
+UserB.friends.push(UserA, UserC, UserD);
+UserC.friends.push(UserA, UserB, UserD);
+UserD.friends.push(UserA, UserB, UserC);
+// saving changes made
+await UserA.save();
+await UserB.save();
+await UserC.save();
+await UserD.save();
+// console logging answers
+console.log("Data was created sucessfully");
+        }else{
+            console.log("data already exists")
+        }
+
+
+
+
         }
     }
 }
