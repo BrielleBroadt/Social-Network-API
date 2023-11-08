b79201b10d88cc441ef468c4ba43c7a6e8564d8a
BrielleBroadt <brielleb.broadt@gmail.com>
2023-11-08 10:52:04 -0500

    post request in thoughtRoutes, trying for new thought

--- Controllers/API/thoughtRoutes.js
+++ Controllers/API/thoughtRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { User, Thought } = require("../../models");
 const mongoose = require("mongoose");
-// get request for thoughts
+// Get request for "thoughts"
 router.get("/", async (req, res) => {
   try {
     const allThoughts = await Thought.find();
@@ -10,3 +10,35 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+// Get request for "thoughtID"
+router.get("/:thoughtId", async (req, res) => {
+    try {
+      const singleThought = await Thought.findOne({ _id: req.params.thoughtId });
+      if (!singleThought) {
+        return res.status(400).json({ message: "No thought with that ID" });
+      }
+      return res.json(singleThought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+// POST requests to create a new Thought and associate it with  User
+  router.post("/", async (req, res) => {
+    try {
+      const newThought = await Thought.create(req.body);
+      const userId = req.body.userId;
+      const user = await User.findById(userId);
+  
+      if (!user) {
+        return res.status(400).json({ message: "No user with that ID" });
+      }
+  
+      user.thoughts.push(newThought._id);
+      await user.save();
+  
+      return res.json(newThought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
